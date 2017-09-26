f14d42d9ccd43da0dd8a8bab965f9b7f220335da
Dione Wilson <dionew1@gmail.com>
2017-09-26 13:01:41 -0600

    First tutorial app

---
+++ test/features/user_sees_a_homepage_test.rb
@@ -0,0 +1,17 @@
+require './test/test_helper'
+
+class HomepageTest < CapybaraTestCase
+  def test_user_can_see_the_homepage
+    visit '/'
+
+    assert page.has_content?("Welcome!")
+    assert_equal 200, page.status_code
+  end
+
+  def test_user_receives_error
+    visit 'blog'
+
+    assert page.has_content?("Page not found.")
+    assert_equal 404, page.status_code
+  end
+end
